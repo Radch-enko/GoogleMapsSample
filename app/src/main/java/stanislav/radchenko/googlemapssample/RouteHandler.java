4711240e67f3813ddb44c179fa1dc35497c7e7ad
stanislav.radchenko <stanislav.radchenko@effective.band>
2022-03-29 00:20:36 +0600

    Initial commit

---
+++ app/src/main/java/stanislav/radchenko/googlemapssample/RouteHandler.java
@@ -0,0 +1,16 @@
+package stanislav.radchenko.googlemapssample;
+
+import android.util.Log;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class RouteHandler {
+
+    static final String url_link = "https://maps.googleapis.com/maps/api/directions/json?";
+
+
+}
